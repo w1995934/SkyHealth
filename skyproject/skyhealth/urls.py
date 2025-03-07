6611120b245189d60a7e9545ec60be00a2e0fbd9
Endrit <feta-eAjetBrahimifeta-e@hotmail.com>
2025-03-07 02:27:49 +0000

    Added static files, URL routing and review page with cards

--- skyproject/skyhealth/urls.py
+++ skyproject/skyhealth/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('login/', views.login),
-    path('nice/', views.home)
+    path('', views.home, name = "skyhealth_home"),
+    path('login/', views.login, name = "skyhealth_login"),
+    path('review/', views.review, name = "skyhealth_review"),
+    path('review/card/<int:id>/', views.card, name = "skyhealth_card")
 ]
--- skyproject/skyhealth/views.py
+++ skyproject/skyhealth/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 
+# Card Data
+cards = [
+    {'id': 1, 'name': "Question 1"},
+    {'id': 2, 'name': "Question 2"},
+    {'id': 3, 'name': "Question 3"},
+    {'id': 4, 'name': "Question 4"},
+    {'id': 5, 'name': "Question 5"}
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'skyhealth/home.html')
 
 def login(request):
     return render(request, 'skyhealth/login.html')
+
+def review(request):
+    context = {'cards':cards}
+    return render(request, 'skyhealth/review.html', context)
+
+def card(request, id):
+    for card in cards:
+        if card['id'] == id:
+            context = {'card': card}
+            break
+    return render(request, 'skyhealth/card.html', context)
